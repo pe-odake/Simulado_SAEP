6b5c50e2e41760664e65e62a5ade850455385f2d
Pedro Odake <pedroodake@gmail.com>
2025-08-06 16:32:22 -0300

    pequenas mudanças e <table> feita

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,18 +28,34 @@ function Home() {
       <div className="table">
         <h2>Bem-vindo!</h2>
         <h2>Turmas Cadastradas</h2>
-        <div className="turmas-grid">
-          {turmas.length === 0 ? (
-            <p>Nenhuma turma cadastrada.</p>
-          ) : (
-            turmas.map((turma) => (
-              <div key={turma.id_turma} className="turma-card">
-                <h3>{turma.nometurma}</h3>
-                <p>ID do Professor: {turma.id_professor}</p>
-                <button onClick={() => deletarTurma(turma.id_turma)}>🗑️</button>
-              </div>
-            ))
-          )}
+        <div className="turmas-table">
+          <table>
+            <thead>
+              <tr>
+                <th>ID Turma</th>
+                <th>Nome da Turma</th>
+                <th>Ano</th>
+                <th>Periodo</th>
+                <th>ID Professor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {turmas.length === 0 ? (
+                <tr>Nenhuma turma cadastrada.</tr>
+              ) : (
+                turmas.map((turma) => (
+                  <tr key={turma.id_turma} className="turma-card">
+                    <td>{turma.id_turma}</td>
+                    <td>{turma.nometurma}</td>
+                    <td>{turma.ano}</td>
+                    <td>{turma.periodo}</td>
+                    <td>ID do Professor: {turma.id_professor}</td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+
         </div>  
       </div>
     </div>
