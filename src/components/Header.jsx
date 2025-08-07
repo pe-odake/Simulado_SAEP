92364957a483bbf8b88fc962d23bdb8c1621abab
Pedro Odake <pedroodake@gmail.com>
2025-08-07 19:04:05 -0300

    tela home, quase finalizada faltando botões para as outras funções e telas com form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import '../styles.css'
-
+import '../css/header.css'
 function Header() {
   return (
     <div className="header">
-        <h1>SAEP</h1>
+        <div className="logo">
+          <img src="image/logo.png" alt="logo" id='logo_img'/>
+          <div id="logo">EduPro</div>
+        </div>
+        <div className="pages">
+          <div id="pages">Home</div>
+          <div id="pages">Turmas</div>
+        </div>
+        <div className="conta">
+          <img src="image/icon.png" alt="account" />
+        </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
-import Botao from '../components/Botao';
+// import Botao from '../components/Botao';
 import Header from '../components/Header';
+import '../css/home.css';
 
 function Home() {
   const [turmas, setTurmas] = useState([])
@@ -25,38 +26,48 @@ function Home() {
   return (
     <div>
       <Header />
-      <div className="table">
-        <h2>Bem-vindo!</h2>
-        <h2>Turmas Cadastradas</h2>
-        <div className="turmas-table">
-          <table>
-            <thead>
-              <tr>
-                <th>ID Turma</th>
-                <th>Nome da Turma</th>
-                <th>Ano</th>
-                <th>Periodo</th>
-                <th>ID Professor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {turmas.length === 0 ? (
-                <tr>Nenhuma turma cadastrada.</tr>
-              ) : (
-                turmas.map((turma) => (
-                  <tr key={turma.id_turma} className="turma-card">
-                    <td>{turma.id_turma}</td>
-                    <td>{turma.nometurma}</td>
-                    <td>{turma.ano}</td>
-                    <td>{turma.periodo}</td>
-                    <td>ID do Professor: {turma.id_professor}</td>
+      <div className="container">
+        <div className="home">
+          <h2>Bem-vindo!</h2>
+          <h2>Turmas Cadastradas</h2>
+          
+          <div className="conteudo">
+            <div className="table-area">
+              <table id='tabela_turmas'>
+                <thead>
+                  <tr>
+                    <th>ID Turma</th>
+                    <th>Nome da Turma</th>
+                    <th>Ano</th>
+                    <th>Periodo</th>
+                    <th>ID Professor</th>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+                </thead>
+                <tbody>
+                  {turmas.length === 0 ? (
+                    <tr><td colSpan="5">Nenhuma turma cadastrada.</td></tr>
+                  ) : (
+                    turmas.map((turma) => (
+                      <tr key={turma.id_turma} className="turma-card">
+                        <td>{turma.id_turma}</td>
+                        <td>{turma.nometurma}</td>
+                        <td>{turma.ano}</td>
+                        <td>{turma.periodo}</td>
+                        <td>ID do Professor: {turma.id_professor}</td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+            </div>
 
-        </div>  
+            <div className="botoes">
+              <button>Adicionar Turma</button>
+              <button>Editar Turma</button>
+              <button>Remover Turma</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
